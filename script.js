bd9be28fbaae27d245e3a16cde8bde1bf224a7fe
rudy gauna <rudy.gauna@gmail.com>
2020-05-09 23:04:53 -0500

    pushing modifications to fix functionality issues

--- script.js
+++ script.js
@@ -1,29 +1,112 @@
-
-
 const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
 
+let shuffledQuestions, currentQuestionIndex
 
-startButton.addEventListener('click', startGame);
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+  currentQuestionIndex++
+  setNextQuestion()
+})
 
-
-
-function startGame(){
- console.log('Started')
- startButton.classList.add('hide')
- questionContainerElement.classList.remove('hide')
-
-
-
-
+function startGame() {
+  startButton.classList.add('hide')
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
+  currentQuestionIndex = 0
+  questionContainerElement.classList.remove('hide')
+  setNextQuestion()
 }
 
-function setNextQuestion(){
-
-
+function setNextQuestion() {
+  resetState()
+  showQuestion(shuffledQuestions[currentQuestionIndex])
 }
 
-function selectAnswer(){
-
-
+function showQuestion(question) {
+  questionElement.innerText = question.question
+  question.answers.forEach(answer => {
+    const button = document.createElement('button')
+    button.innerText = answer.text
+    button.classList.add('btn')
+    if (answer.correct) {
+      button.dataset.correct = answer.correct
+    }
+    button.addEventListener('click', selectAnswer)
+    answerButtonsElement.appendChild(button)
+  })
 }
+
+function resetState() {
+  clearStatusClass(document.body)
+  nextButton.classList.add('hide')
+  while (answerButtonsElement.firstChild) {
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+  }
+}
+
+function selectAnswer(e) {
+  const selectedButton = e.target
+  const correct = selectedButton.dataset.correct
+  setStatusClass(document.body, correct)
+  Array.from(answerButtonsElement.children).forEach(button => {
+    setStatusClass(button, button.dataset.correct)
+  })
+  if (shuffledQuestions.length > currentQuestionIndex + 1) {
+    nextButton.classList.remove('hide')
+  } else {
+    startButton.innerText = 'Restart'
+    startButton.classList.remove('hide')
+  }
+}
+
+function setStatusClass(element, correct) {
+  clearStatusClass(element)
+  if (correct) {
+    element.classList.add('correct')
+  } else {
+    element.classList.add('wrong')
+  }
+}
+
+function clearStatusClass(element) {
+  element.classList.remove('correct')
+  element.classList.remove('wrong')
+}
+
+const questions = [
+  {
+    question: 'True or False: In the Star Wars Univers, Jango & Boba Fett are not considered REAL Mandalorians.',
+    answers: [
+      { text: 'True', correct: true },
+      { text: 'False', correct: false }
+    ]
+  },
+  {
+    question: 'Which former American physicist claimed to who have work on a projects reverse engineering extraterrestrial craft?',
+    answers: [
+      { text: 'Bob Barker', correct: false },
+      { text: 'Ricky Bobby', correct: false },
+      { text: 'Bob Lazar', correct: true },
+      { text: 'Fun Fun Function', correct: false }
+    ]
+  },
+  {
+    question: 'What year was the first programmable drum machine invented?',
+    answers: [
+      { text: '1992', correct: false },
+      { text: '1206', correct: true },
+      { text: '1313', correct: false },
+      { text: '1975', correct: false }
+    ]
+  },
+  {
+    question: "Is pimpin', EZ?",
+    answers: [
+      { text: 'Nah', correct: true },
+      { text: 'Yeah BoI', correct: true }
+    ]
+  }
+]
